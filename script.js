bbd42838cd3b1fdc574b57fc104d716e843ca378
kohei <ykohei46@outlook.jp>
2025-02-02 04:51:14 +0900

    git push "node.js"

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// let name = "航平";
-// let age = 23;
-// let isstudent = false;
-// console.log(name + "の年齢は" + age + "です。");
+console.log("スクリプトは動作しています！");
+let name = "航平";
+let age = 23;
+let isstudent = false;
+console.log(name + "の年齢は" + age + "です。");
 
-let namae = "太郎";
-let age = 25;
-let isStudent = true;
-console.log(namae + "の年齢は" + age + "歳です");
+
 
