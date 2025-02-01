a7ce16a43cd2653ba97bf209a64d901f566e3a61
kohei <ykohei46@outlook.jp>
2025-02-02 01:48:16 +0900

    node.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // let isstudent = false;
 // console.log(name + "の年齢は" + age + "です。");
 
-let name = "太郎";
+let namae = "太郎";
 let age = 25;
 let isStudent = true;
-console.log(name + "の年齢は" + age + "歳です");
+console.log(namae + "の年齢は" + age + "歳です");
 
