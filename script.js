b742dba7791e73b82fd18b44a64b521628b5d2e9
kohei <ykohei46@outlook.jp>
2025-02-02 00:29:44 +0900

    javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+// let name = "航平";
+// let age = 23;
+// let isstudent = false;
+// console.log(name + "の年齢は" + age + "です。");
+
+let name = "太郎";
+let age = 25;
+let isStudent = true;
+console.log(name + "の年齢は" + age + "歳です");
+
