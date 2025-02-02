3aa0a07f75afa2834609496eb604d0d0952533b0
kohei <ykohei46@outlook.jp>
2025-02-03 01:07:47 +0900

    javascript

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ let age = 23;
 let isstudent = false;
 console.log(name + "の年齢は" + age + "です。");
 
+console.log("hello world");
 
+if (age >= 20) {
+    console.log("私は成人してます。");
+} else {
+    console.log("私は成人していません。")
+}
 
+for (const i = 0;i <= 5;i ++ ) {
+    console.log(i)
+}
